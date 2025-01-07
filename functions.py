371e92f15eaff0ab879abc44f4bee2b720be1859
sita <138875606+sita26@users.noreply.github.com>
2025-01-08 00:08:53 +0545

    commit

---
+++ functions.py
@@ -0,0 +1,13 @@
+
+# x  = [22, 44, 28, 29, 8]
+# print(max(x))
+
+
+
+# x = [2.43, 4.568, 6.34, 9.33, 2.99]
+# print(round(4.568, 2))
+
+
+x = ['mom', 'dad', 'bro', 'sis', 'frd']
+# print(x.index('bro'))
+print(x.count('sis'))
